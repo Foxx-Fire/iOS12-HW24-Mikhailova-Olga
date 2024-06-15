61186e857ac8b01307853077cccbc18fcd214755
Olga Mikhailova <foxx_fire@mail.ru>
2024-06-15 14:11:53 +0300

    Initial Commit

---
+++ iOS12-HW24-Mikhailova Olga/iOS12_HW24_Mikhailova_OlgaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS12_HW24_Mikhailova_OlgaApp.swift
+//  iOS12-HW24-Mikhailova Olga
+//
+//  Created by FoxxFire on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS12_HW24_Mikhailova_OlgaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
